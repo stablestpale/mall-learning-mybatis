767bc70c5c824404ba52d9e4d0f5770e8456119f
stablestaple <sdzzy17186@163.com>
2021-07-12 10:03:34 +0800

    初始化项目

---
+++ src/test/java/mall_mybatis/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mall_mybatis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
